e437e47d05b9bc4f699903a5180946b5a877c0b9
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-04-27 17:20:36 -0300

    utilizando default method, lambda e method reference

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class Curso {
@@ -27,5 +28,8 @@ public class ExemploCursos {
         cursos.add(new Curso("JavaScript", 150));
         cursos.add(new Curso("Java 8", 113));
         cursos.add(new Curso("C", 55));
+
+        cursos.sort(Comparator.comparing(Curso::getAlunos));
+        cursos.forEach(c -> System.out.println(c.getNome()));
     }
 }
