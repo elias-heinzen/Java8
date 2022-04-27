0f09a0d0e309d40e7446baa4028c228bf63f9b26
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-04-27 16:17:57 -0300

    usando lambda no sort

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -12,24 +12,16 @@ public class OrdenaStrings {
         palavras.add("editora casa do codigo");
         palavras.add("caelum");
 
-        Comparator<String> comparador = new ComparadorPorTamanho();
-
-        palavras.sort(comparador);
+        palavras.sort((s1, s2) -> {
+            if (s1.length() < s2.length())
+                return -1;
+            if (s1.length() > s2.length())
+                return 1;
+            return 0;
+        });
 
         System.out.println(palavras);
 
         palavras.forEach(s -> System.out.println(s));
-    }
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-        if (s1.length() < s2.length())
-            return -1;
-        if (s1.length() > s2.length())
-            return 1;
-        return 0;
     }
 }
