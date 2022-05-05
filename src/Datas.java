3c1eaa0437f2561b3e3086fb16171c9d4c75463f
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-05-04 21:26:38 -0300

    utilizando LocalDateTime e LocalTime

--- src/Datas.java
+++ src/Datas.java
@@ -1,6 +1,7 @@
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -26,5 +27,13 @@ public class Datas {
 
         String valorFormatado = proximasOlimpiadas.format(formatador);
         System.out.println(valorFormatado);
+
+        DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora.format(formatadorComHoras));
+
+        LocalTime intervalo = LocalTime.of(15, 30);
+        System.out.println(intervalo);
     }
 }
